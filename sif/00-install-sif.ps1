aafb73bf6564937a1b591a6493c2d2763423f4e8
Adrian Stauffer <adrian.stauffer@infocentric.ch>
2019-05-13 15:59:02 +0200

    set specific SIF version to install

--- sif/00-install-sif.ps1
+++ sif/00-install-sif.ps1
@@ -1,3 +1,3 @@
 Register-PSRepository -Name SitecoreGallery -SourceLocation https://sitecore.myget.org/F/sc-powershell/api/v2
-Install-Module SitecoreInstallFramework
+Install-Module -Name SitecoreInstallFramework -Repository SitecoreGallery -RequiredVersion 2.0.0
 Update-Module SitecoreInstallFramework
